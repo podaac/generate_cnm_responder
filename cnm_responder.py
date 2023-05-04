b99780ca2039602d435a4be01ddabf747733c267
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-05-04 15:12:24 +0000

    Test for changes 1

--- cnm_responder.py
+++ cnm_responder.py
@@ -1,4 +1,4 @@
-"""AWS Lambda that handles CNM responses published to an SNS Topic.
+"""CHANGED? AWS Lambda that handles CNM responses published to an SNS Topic.
 
 Deletes successfully ingested L2P granules from S3 bucket and EFS.
 Logs the errors.
